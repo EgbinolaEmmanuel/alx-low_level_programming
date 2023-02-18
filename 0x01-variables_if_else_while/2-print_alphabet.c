51b1fb42c01778889060dc24a93d0a9ddaa4e916
Immanuellaurel <oloruntobaemanuel@gmail.com>
2023-02-17 19:10:38 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,18 +2,20 @@
 
 /**
  * main - prints the alphabet in lowercase, and then in uppercase,
+ *
  * followed by a new line
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int ch;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}	
 	putchar('\n');
 	return (0);
 }
-
